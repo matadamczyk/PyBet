13fd18c862dd1b3a4d181e873bc6ab6265c02753
BartlomiejRydzak <152024463+BartlomiejRydzak@users.noreply.github.com>
2024-12-06 11:47:58 +0100

    Update sts.py

--- backend/datacollection/bookmakers/sts.py
+++ backend/datacollection/bookmakers/sts.py
@@ -2,15 +2,52 @@ from bs4 import BeautifulSoup
 import requests
 
 
+
+def get_bts(url):
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+    from selenium.webdriver.chrome.options import Options
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+    chrome_options.add_argument("--disable-gpu")  # Optional: for compatibility with some environments
+    chrome_options.add_argument("--no-sandbox")  # Optional: for environments like Linux servers
+
+    # Set up Selenium WebDriver
+    driver = webdriver.Chrome(options=chrome_options)  # Ensure the WebDriver path is correct
+    driver.get(url)
+
+    try:
+        # Wait for the parent element to be visible
+        wait = WebDriverWait(driver, 30)
+        parent_element = wait.until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/app-mweb/div/div/div/div[1]/div/div[2]/app-match-detail-prematch/app-match-detail/div/div[4]/app-prematch-odds/div/app-odds-page/app-match-details-group[2]"))
+        )
+        
+        # Retrieve all text within the parent element
+        text_content = parent_element.text.split("\n")
+
+        # Print the extracted text
+        # print("Extracted Text:")
+        # print(text_content)
+
+    except Exception as e:
+        print("An error occurred:", e)
+
+    finally:
+        driver.quit()
+
+    return (text_content[2],text_content[4])
+
+
 def get_data(url):
     response = requests.get(url)
     content = response.text
 
     soup = BeautifulSoup(content, "html.parser")
 
-    team_list = soup.find_all(
-        class_="match-tile-event-details-teams__team match-tile-event-details-teams__team--1"
-    )
+    team_list = soup.find_all(class_="match-tile-event-details-teams__team match-tile-event-details-teams__team--1")
 
     # "match-tile-event-details-teams__team match-tile-event-details-teams__team--1"
 
@@ -30,9 +67,7 @@ def get_data(url):
         text = team.getText()
         teams1.append(text)
 
-    team_list = soup.find_all(
-        class_="match-tile-event-details-teams__team match-tile-event-details-teams__team--2"
-    )
+    team_list = soup.find_all(class_="match-tile-event-details-teams__team match-tile-event-details-teams__team--2")
 
     for team in team_list:
         text = team.getText()
@@ -41,15 +76,40 @@ def get_data(url):
     course_data = soup.find_all(class_="odds-button__odd-value ng-star-inserted")
 
     flag = True
-    i = 0
+    i=0
     for course in course_data:
-        if i == 3:
-            i = 0
+        if i==3:
+            i=0
             flag = not flag
-        if flag:
+        if(flag):
             text = course.getText()
             courses.append(text)
-        i += 1
+        i+=1
+
+    tile_list = soup.find_all("bb-prematch-match-tile", class_="ng-star-inserted")
+
+    links = []
+    bts = []
+
+    for tile in tile_list:
+        link = tile.find("a")  
+        if link:  
+            url = link.get("href")
+            links.append(url)
+        
+    for link in links:
+        # print(f"https://www.sts.pl{link}")
+        bts.append(get_bts(f"https://www.sts.pl{link}"))
+        # content = response.text
+
+        # soup = BeautifulSoup(content, "html.parser")
+        # print(response)
+        # goal = soup.find_all(name="span", class_="odds-button__odd-value ng-star-inserted")
+        # print(goal)
+        # for i in goal:
+        #     print(i)
+        #     print(i.getText())
+        
 
     data = []
 
@@ -59,29 +119,28 @@ def get_data(url):
             "team1": teams1[i],
             "team2": teams2[i],
             "course1": courses[i],
-            "courseX": courses[i + 1],
-            "course2": courses[i + 2],
+            "courseX": courses[i+1],
+            "course2": courses[i+2],
+            "bts": bts[i][0],
+            "nbts": bts[i][1]
         }
 
         data.append(temp)
 
     return data
 
-
 if __name__ == "__main__":
-    data = get_data(
-        "https://www.sts.pl/zaklady-bukmacherskie/pilka-nozna/anglia/premier-league/184/30862/86451"
-    )
+    data = get_data("https://www.sts.pl/zaklady-bukmacherskie/pilka-nozna/anglia/premier-league/184/30862/86451")
 
     print(data)
 
     print()
 
-    data = get_data(
-        "https://www.sts.pl/zaklady-bukmacherskie/pilka-nozna/anglia/2-liga/184/30862/86456"
-    )
+    data = get_data("https://www.sts.pl/zaklady-bukmacherskie/pilka-nozna/anglia/2-liga/184/30862/86456")
 
     print(data)
+
+
 
 
 # response = requests.get("https://www.sts.pl/zaklady-bukmacherskie/pilka-nozna/anglia/2-liga/184/30862/86456")
@@ -94,19 +153,21 @@ if __name__ == "__main__":
 # links = []
 
 # for tile in tile_list:
-#     link = tile.find("a")
-#     if link:
+#     link = tile.find("a")  
+#     if link:  
 #         url = link.get("href")
 #         links.append(url)
-
+    
 # for link in links:
-#     response = requests.get(f"https://www.sts.pl{link}")
-#     content = response.text
+#     print(f"https://www.sts.pl{link}")
+#     response = get_bts(f"https://www.sts.pl{link}")
+#     # content = response.text
 
-#     soup = BeautifulSoup(content, "html.parser")
-
-#     goal = soup.find_all(name="span", class_="odds-button__odd-value ng-star-inserted")
-#     print(goal)
-#     for i in goal:
-#         print(i)
-#         print(i.getText())
+#     # soup = BeautifulSoup(content, "html.parser")
+#     print(response)
+#     # goal = soup.find_all(name="span", class_="odds-button__odd-value ng-star-inserted")
+#     # print(goal)
+#     # for i in goal:
+#     #     print(i)
+#     #     print(i.getText())
+    
