98e3133eaf35ef7b08f5ca542f3362f80cf3de20
Mateusz Adamczyk <madamczyk@student.agh.edu.pl>
2025-01-31 12:43:50 +0100

    fix: fixed tests confing

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="project_name",
-    version="0.1",
+    name="pybet",
+    version="0.0.1",
     packages=find_packages(),
+    install_requires=[
+        'django',
+        'requests',
+        'beautifulsoup4',
+        'numpy',
+        'scikit-learn',
+        'joblib',
+    ],
 )
